fcc470b303b57b4227f24b64dc94e3fbdaa0ace8
arthur silva <arxdsilva@gmail.com>
2018-09-21 17:28:01 -0300

    tem asas e pulga

--- main.cpp
+++ main.cpp
@@ -114,6 +114,11 @@ char const* animalTree() {
         resp = "jacare\n";
         return resp;
     }
+    cout << "possui esqueleto? [0,1] \n";
+    cin >> consoleResp;
+    if (consoleResp == 1) {
+        // fazer
+    }
     return resp;
 }
 

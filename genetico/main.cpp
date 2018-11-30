d00d7713a9fe7aef353495bedf013ebcfcddc2d0
Arthur Silva <arxdsilva@gmail.com>
2018-11-30 13:30:17 -0200

    fix var name

--- genetico/main.cpp
+++ genetico/main.cpp
@@ -54,7 +54,7 @@ void generateMaze() {
 void initMaze() {
     for ( int i = 0; i < size; i++ ) {
         for ( int j = 0; j < size; j++ ) {
-            grid[i][j] = 0;
+            maze[i][j] = 0;
         }
     }
 }
